4910b5ebe69391888ef46315cf34e04c3dd6a70c
randyhdev <72906202+randyhdev@users.noreply.github.com>
2021-03-31 13:12:44 -0400

    update testing
    update vagrant testing to unzip output

--- tests/testing.sh
+++ tests/testing.sh
@@ -11,13 +11,15 @@ GOOS=linux GOARCH=amd64 go build -o host-diagnostics_linux
 cd ./tests/vagrant_ubuntu && vagrant up
 
 vagrant ssh -c "/home/vagrant/host-diagnostics_linux"
-vagrant ssh -c "cat /home/vagrant/results.json"
+vagrant ssh -c "gunzip ./support.tar.gz && tar xopf support.tar --strip-components=1"
+vagrant ssh -c "cat /home/vagrant/HostInfo.json"
 vagrant destroy -f
 
 # spin up rhel vm, run binary, check output, tear down
 cd ../vagrant_rhel && vagrant up
 vagrant ssh -c "/home/vagrant/host-diagnostics_linux"
-vagrant ssh -c "cat /home/vagrant/results.json"
+vagrant ssh -c "gunzip ./support.tar.gz && tar xopf support.tar --strip-components=1"
+vagrant ssh -c "cat /home/vagrant/HostInfo.json"
 vagrant destroy -f
 
 cd ..
