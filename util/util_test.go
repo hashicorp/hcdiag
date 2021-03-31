fca180cf35096b9a698e721f715d1a2ac140368b
randyhdev <72906202+randyhdev@users.noreply.github.com>
2021-03-31 16:47:02 -0400

    create a basic test for util
    currently just a couple basic tests for executecommands
    
    need to add targz, maptojson, jsontofile

---
+++ util/util_test.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestExecuteCommands(t *testing.T) {
+	// Create list of command struct to execute
+	testCommandStruct := []CommandStruct{{
+		Attribute: "test",
+		Command:   "echo",
+		Arguments: []string{"test"},
+		Format:    "string",
+	}}
+
+	// Craft expected result for comparison
+	expectedResult := map[string]interface{}{
+		"test": "test",
+	}
+
+	// Execute command and validate no error
+	result, err := ExecuteCommands(testCommandStruct, false)
+	if err != nil {
+		t.Errorf("TestExecuteCommands failed: %s", err)
+	}
+
+	// Validate expected result
+	if result["test"] != expectedResult["test"] {
+		t.Errorf("Unexpected result: %s", result)
+	}
+}
+
+// TarGz(sourceDir string, destFileName string) error
+
+// MapToJSON(mapVar map[string]interface{}) ([]byte, error)
+
+// JSONToFile(JSON []byte, outFile string) error
