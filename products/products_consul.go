da4ef7401902871a4dd96d084dd7e5dd70eb0801
randyhdev <72906202+randyhdev@users.noreply.github.com>
2021-04-15 09:15:24 -0400

    remove consul file output for now
    remove consul file output for now

--- products/products_consul.go
+++ products/products_consul.go
@@ -9,9 +9,9 @@ import (
 // ConsulSeekers seek information about Consul.
 func ConsulSeekers(tmpDir string) []*s.Seeker {
 	return []*s.Seeker{
-		s.NewCommander(fmt.Sprintf("consul debug -output=%s/ConsulDebug.tar.gz -duration=%ds -interval=%ds", tmpDir, DebugSeconds, IntervalSeconds), "string", false),
-		s.NewCommander("consul members >> %s/consul-members.txt", "string", false),
-		s.NewCommander("consul operator raft list-peers >> %s/consul-peers.txt", "string", false),
-		s.NewCommander("consul info >> %s/consul-info.txt", "string", false),
+		s.NewCommander("consul info", "string", false),
+		s.NewCommander("consul members", "string", false),
+		s.NewCommander("consul operator raft list-peers", "string", false),
+		s.NewCommander(fmt.Sprintf("consul debug -output=%s/ConsulDebug -duration=%ds -interval=%ds", tmpDir, DebugSeconds, IntervalSeconds), "string", false),
 	}
 }
