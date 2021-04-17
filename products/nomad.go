0d259a048591a78fa2860ded5c555910c7d55da1
Daniel Bennett <dbennett@hashicorp.com>
2021-04-16 22:45:00 -0500

    replace some nomad commands with api calls
    the cli calls have varying non-json outputs,
    but their api calls are consistently json

--- products/nomad.go
+++ products/nomad.go
@@ -3,20 +3,23 @@ package products
 import (
 	"fmt"
 
+	"github.com/hashicorp/host-diagnostics/apiclients"
 	s "github.com/hashicorp/host-diagnostics/seeker"
 )
 
 // NomadSeekers seek information about Nomad.
 func NomadSeekers(tmpDir string) []*s.Seeker {
+	api := apiclients.NewNomadAPI()
 	return []*s.Seeker{
 		s.NewCommander("nomad version", "string", true),
-		s.NewCommander("nomad plugin status", "string", false),
-		s.NewCommander("nomad server members -detailed", "string", false),
 		s.NewCommander("nomad node status -json", "json", false),
-		s.NewCommander("nomad operator raft list-peers", "string", false),
-		s.NewCommander("nomad operator autopilot get-config", "string", false),
 		s.NewCommander("nomad agent-info -json", "json", false),
 		s.NewCommander(fmt.Sprintf("nomad operator debug -output=%s -duration=%ds", tmpDir, DebugSeconds), "string", false),
 		// s.NewCommander("nomad operator metrics", "json", false), // TODO: uncomment (it's very verbose, so not noisy during testing)
+
+		s.NewHTTPer(api, "/v1/agent/members", false),
+		s.NewHTTPer(api, "/v1/plugins?type=csi", false),
+		s.NewHTTPer(api, "/v1/operator/autopilot/configuration", false),
+		s.NewHTTPer(api, "/v1/operator/raft/configuration", false),
 	}
 }
