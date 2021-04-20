2abc884d6dd79f714a99344cae3df9e8d5c8e7dc
randyhdev <72906202+randyhdev@users.noreply.github.com>
2021-04-20 11:19:26 -0400

    remove temp comments
    remove temp comments, add to shared notes doc

--- products/consul.go
+++ products/consul.go
@@ -13,28 +13,12 @@ func ConsulSeekers(tmpDir string) []*s.Seeker {
 	return []*s.Seeker{
 		s.NewCommander(fmt.Sprintf("consul debug -output=%s/ConsulDebug -duration=%ds -interval=%ds", tmpDir, DebugSeconds, IntervalSeconds), "string", false),
 
-		// TODO: consider adding description/purpose attribute to runner structs
-		s.NewHTTPer(api, "/v1/agent/self", false),          // config and member info of local agent
-		s.NewHTTPer(api, "/v1/agent/metrics", false),       // metrics for most recent finished interval
-		s.NewHTTPer(api, "/v1/catalog/datacenters", false), // list of known datacenters
-		s.NewHTTPer(api, "/v1/catalog/services", false),    // list of registered services, consider accepting 'dc' and 'ns' params
-		s.NewHTTPer(api, "/v1/namespace", false),           // list all Namespaces (enterprise)
-		s.NewHTTPer(api, "/v1/status/leader", false),       // get Raft leader for dc
-		s.NewHTTPer(api, "/v1/status/peers", false),        // get Raft peers for dc
-
-		// TODO:
-		// consider allowing service param to enable /catalog/service/:service, /catalog/connect/:service
-		// consider allowing node param to enable /catalog/node-services/:node
-		// consider allowing gateway param to enable /catalog/gateway-services/:gateway
-		//	https://www.consul.io/api-docs/catalog
-
-		// consider allowing kind param to enable /config/:kind, or add each possible separately e.g. /config/service-defaults
-		//	https://www.consul.io/api-docs/config
-
-		// params (or assume all) also enables /health endpoints
-
-		// consul config discovery
-		// consul log discovery
-		// agent/self result DebugConfig/Logging/LogFilePath ?
+		s.NewHTTPer(api, "/v1/agent/self", false),
+		s.NewHTTPer(api, "/v1/agent/metrics", false),
+		s.NewHTTPer(api, "/v1/catalog/datacenters", false),
+		s.NewHTTPer(api, "/v1/catalog/services", false),
+		s.NewHTTPer(api, "/v1/namespace", false),
+		s.NewHTTPer(api, "/v1/status/leader", false),
+		s.NewHTTPer(api, "/v1/status/peers", false),
 	}
 }
