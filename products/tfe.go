4d0c8f02ce7da921ec65690a28dcb83ab4507f69
randyhdev <rhodges@hashicorp.com>
2021-05-06 14:17:30 -0400

    add more tfe commands
    add a few more tfe api commands

--- products/tfe.go
+++ products/tfe.go
@@ -14,6 +14,13 @@ func TFESeekers(tmpDir string) []*s.Seeker {
 
 		s.NewCopier("/var/lib/replicated/support-bundles", "replicated-support*.tar.gz", tmpDir, false),
 
+		s.NewHTTPer(api, "/api/v2/admin/cost-estimation-settings", false),
+		s.NewHTTPer(api, "/api/v2/admin/customization-settings", false),
 		s.NewHTTPer(api, "/api/v2/admin/general-settings", false),
+		s.NewHTTPer(api, "/api/v2/admin/organizations", false),
+		s.NewHTTPer(api, "/api/v2/admin/saml-settings", false),
+		s.NewHTTPer(api, "/api/v2/admin/smtp-settings", false),
+		s.NewHTTPer(api, "/api/v2/admin/terraform-versions", false),
+		s.NewHTTPer(api, "/api/v2/admin/twilio-settings", false),
 	}
 }
