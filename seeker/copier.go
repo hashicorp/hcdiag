bf1884da1f5325b86a1924eb22c51e1ed4ab0137
randyhdev <rhodges@hashicorp.com>
2021-05-07 12:46:48 -0400

    update Copier variables
    update copier variables, change temp to dest, change json formatting

--- seeker/copier.go
+++ seeker/copier.go
@@ -5,29 +5,29 @@ import (
 )
 
 // NewCopier provides a Seeker for copying files to temp dir based on a filter.
-func NewCopier(srcDir string, filter string, tmpDir string, mustSucceed bool) *Seeker {
+func NewCopier(SourceDir string, Filter string, DestDir string, mustSucceed bool) *Seeker {
 	return &Seeker{
-		Identifier:  "copy " + srcDir + "/" + filter,
-		Runner:      Copier{srcDir: srcDir, filter: filter, tmpDir: tmpDir},
+		Identifier:  "copy " + SourceDir + "/" + Filter,
+		Runner:      Copier{SourceDir: SourceDir, Filter: Filter, DestDir: DestDir},
 		MustSucceed: mustSucceed,
 	}
 }
 
 // Copier copies files to temp dir based on a filter.
 type Copier struct {
-	srcDir string `json:"srcDir"`
-	filter string `json:"filter"`
-	tmpDir string `json:"tmpDir"`
+	SourceDir string `json:"source_directory"`
+	Filter    string `json:"filter"`
+	DestDir   string `json:"destination_directory"`
 }
 
 func (c Copier) Run() (result interface{}, err error) {
-	files, err := util.FilterWalk(c.srcDir, c.filter)
+	files, err := util.FilterWalk(c.SourceDir, c.Filter)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, s := range files {
-		err := util.CopyDir(c.tmpDir, s)
+		err := util.CopyDir(c.DestDir, s)
 		if err != nil {
 			return nil, err
 		}
