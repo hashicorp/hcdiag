e6e5d2d4364697f45f3abd8b33de1f8c27b9bd9d
Daniel Bennett <dbennett@hashicorp.com>
2021-05-14 09:54:23 -0500

    bonus: remove redundant help text turns out the default behavior for flag.StringVar() is to include the default value in the help text

--- diagnosticator.go
+++ diagnosticator.go
@@ -85,7 +85,7 @@ func (f *Flags) ParseFlags(args []string) {
 	flags.BoolVar(&f.Vault, "vault", false, "Run Vault diagnostics")
 	flags.BoolVar(&f.AllProducts, "all", false, "Run all available product diagnostics")
 	flags.Var(&CSVFlag{&f.Includes}, "includes", "files or directories to include (comma-separated, file-*-globbing available if 'wrapped-*-in-single-quotes')\ne.g. '/var/log/consul-*,/var/log/nomad-*'")
-	flags.StringVar(&f.Outfile, "outfile", "support.tar.gz", "Output file name (default: support.tar.gz)")
+	flags.StringVar(&f.Outfile, "outfile", "support.tar.gz", "Output file name")
 	flags.Parse(args)
 }
 
