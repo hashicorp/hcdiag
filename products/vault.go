54153f4478ae37031da42a6c2f4bf061e2f8ffa8
Kit Patella <kit@jepsen.io>
2021-05-14 14:58:29 -0700

    products: fix the build error from renaming the defaults

--- products/vault.go
+++ products/vault.go
@@ -14,6 +14,6 @@ func VaultSeekers(tmpDir string) []*s.Seeker {
 		s.NewCommander("vault read sys/health -format=json", "json", false),
 		s.NewCommander("vault read sys/seal-status -format=json", "json", false),
 		s.NewCommander("vault read sys/host-info -format=json", "json", false),
-		s.NewCommander(fmt.Sprintf("vault debug -output=%s/VaultDebug.tar.gz -duration=%ds", tmpDir, DebugSeconds), "string", false),
+		s.NewCommander(fmt.Sprintf("vault debug -output=%s/VaultDebug.tar.gz -duration=%ds", tmpDir, DefaultDebugSeconds), "string", false),
 	}
 }
