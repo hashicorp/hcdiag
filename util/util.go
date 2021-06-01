34d45fab849a66380c69f771f691cf5369309bbe
Kit Patella <kit@jepsen.io>
2021-06-01 12:42:46 -0700

    fix isInRange and set up a future fix later? this might be bugged

--- util/util.go
+++ util/util.go
@@ -131,23 +131,34 @@ func SplitFilepath(path string) (dir string, file string) {
 }
 
 func isInRange(path string, from, to time.Time) (bool, error) {
+	// Default true if no range provided
+	if !from.IsZero() {
+		return true, nil
+	}
+
 	// When we only get a `from` value, the `to` is now
 	if to.IsZero() {
 		to = time.Now()
 	}
-	// Default true if no range provided
-	if !from.IsZero() {
-		info, err := os.Stat(path)
-		if err != nil {
-			return false, err
-		}
-		mod := info.ModTime()
-		// Has our file been modified within the time range?
-		if mod.After(to) && mod.Before(from) {
-			return true, nil
-		}
+
+	// Grab our file's last modified time
+	info, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	mod := info.ModTime()
+
+	// Check if the mod time is outside of the range
+	// NOTE(mkcp): Can this "after" check bug if there's no range provided, so we set it to "now" and the file is being
+	//  updated in parallel? Would that mean the modified time becomes _after_ Now even though we've statted the file?
+	//  There's no read snapshot happening of the file... maybe we should completely cut the "after" check if it's zero
+	//  rather than fudging a range check with a defaulted value. That's more semantic anyway, considering we're not
+	//  checking a range at all, but just a before
+	if mod.Before(from) || mod.After(to) {
 		return false, nil
 	}
+
+	// Yes, it's in the range!
 	return true, nil
 }
 
