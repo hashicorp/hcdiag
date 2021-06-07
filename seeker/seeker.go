79ecbf53a6030dc10fd1e8011da3d48f005334fd
Kit Patella <kit@jepsen.io>
2021-06-07 15:47:36 -0700

    seekers: add functions to exclude and select seekers

--- seeker/seeker.go
+++ seeker/seeker.go
@@ -29,3 +29,48 @@ func (s *Seeker) Run() (result interface{}, err error) {
 	}
 	return result, err
 }
+
+// Exclude takes a slice of matcher strings and a slice of seekers. If any of the seeker identifiers match the exclude
+// exactly then it will not be present in the returned seeker slice.
+// NOTE(mkcp): This is precisely identical to Select() except we flip the match check. Maybe we can perform both rounds
+//  of filtering in one pass one rather than iterating over all the seekers several times. Not likely to be a huge speed
+//  increase though... we're not even remotely bottlenecked on seeker filtering.
+func Exclude(excludes []string, seekers []*Seeker) []*Seeker {
+	var newSeekers []*Seeker
+	for _, s := range seekers {
+		// Set our match flag if we get a hit for any of the matchers on this seeker
+		var match bool
+		for _, matcher := range excludes {
+			if s.Identifier == matcher {
+				match = true
+			}
+		}
+
+		// Add the seeker back to our set if we have not matched an exclude
+		if !match {
+			newSeekers = append(newSeekers, s)
+		}
+	}
+	return newSeekers
+}
+
+// Select takes a slice of matcher strings and a slice of seekers. The only seekers returned will be those exactly
+// matching the given select strings.
+func Select(selects []string, seekers []*Seeker) []*Seeker {
+	var newSeekers []*Seeker
+	for _, s := range seekers {
+		// Set our match flag if we get a hit for any of the matchers on this seeker
+		var match bool
+		for _, matcher := range selects {
+			if s.Identifier == matcher {
+				match = true
+			}
+		}
+
+		// Only include the seeker if we've matched it
+		if match {
+			newSeekers = append(newSeekers, s)
+		}
+	}
+	return newSeekers
+}
