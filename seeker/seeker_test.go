535f7c11ad1736318196dadd44171a384ff3c152
Daniel Bennett <dbennett@hashicorp.com>
2022-02-07 20:38:59 -0600

    refactor seeker test and mock runner

--- seeker/seeker_test.go
+++ seeker/seeker_test.go
@@ -2,7 +2,6 @@ package seeker
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,38 +12,40 @@ var (
 	errFake    = errors.New("uh oh a fake error")
 )
 
-type MockRunner struct{}
+func NewMockRunner() *MockRunner {
+	return &MockRunner{
+		result: mockResult,
+	}
+}
+
+type MockRunner struct {
+	result string
+	err    error
+}
 
 func (r MockRunner) Run() (interface{}, error) {
-	return mockResult, errFake
+	return r.result, r.err
 }
 
 func TestSeekerRun(t *testing.T) {
-	r := MockRunner{}
+	r := NewMockRunner()
 	s := Seeker{Identifier: "mock", Runner: r}
 
+	// no error
 	result, err := s.Run()
+	assert.Equal(t, mockResult, result)
+	assert.Equal(t, mockResult, s.Result)
+	assert.NoError(t, err)
+	assert.NoError(t, s.Error)
+	assert.Equal(t, Success, s.Status)
 
-	// assert that return values are also being stored as struct fields
-	if s.Result != result {
-		t.Errorf("returned result (%s) does not match Seeker Result field (%s)", result, s.Result)
-	}
-	if s.Error != err {
-		t.Errorf("returned err (%s) does not match Seeker Error field (%s)", err, s.Error)
-	}
-	errStr := fmt.Sprintf("%s", err)
-	if s.ErrString != errStr {
-		t.Errorf("Seeker ErrString (%s) not formatted as expected (%s)", s.ErrString, errStr)
-	}
-
-	// assert that values are what we expect from MockRunner.Run()
-	if err != errFake {
-		t.Errorf("err should be '%s', got: '%s'", errFake, err)
-	}
-	if result != mockResult {
-		t.Errorf("resp should be '%s'; got '%s'", mockResult, result)
-	}
-
+	// with error
+	r.err = errFake
+	_, err = s.Run()
+	assert.ErrorIs(t, err, errFake)
+	assert.ErrorIs(t, s.Error, errFake)
+	assert.Equal(t, errFake.Error(), s.ErrString)
+	assert.Equal(t, Fail, s.Status)
 }
 
 func TestExclude(t *testing.T) {
