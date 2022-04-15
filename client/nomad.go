fb5609b7c431356cf6597401252be6bbc9a83950
Nick Chandler <nick.chandler@hashicorp.com>
2022-04-15 15:56:47 -0400

    initial nomad tls config

--- client/nomad.go
+++ client/nomad.go
@@ -6,10 +6,20 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-const DefaultNomadAddr = "http://127.0.0.1:4646"
+const (
+	DefaultNomadAddr = "http://127.0.0.1:4646"
+
+	EnvNomadCaCert        = "NOMAD_CACERT"
+	EnvNomadCaPath        = "NOMAD_CAPATH"
+	EnvNomadClientCert    = "NOMAD_CLIENT_CERT"
+	EnvNomadClientKey     = "NOMAD_CLIENT_KEY"
+	EnvNomadSkipVerify    = "NOMAD_SKIP_VERIFY"
+	EnvNomadTlsServerName = "NOMAD_TLS_SERVER_NAME"
+)
 
 // NewNomadAPI returns an APIClient for Nomad.
 func NewNomadAPI() *APIClient {
@@ -25,6 +35,43 @@ func NewNomadAPI() *APIClient {
 	}
 
 	return NewAPIClient("nomad", addr, headers)
+}
+
+// NewNomadTLSConfig returns a *TLSConfig object, using
+// default environment variables to build up the object.
+func NewNomadTLSConfig() (*TLSConfig, error) {
+	tlsConfig := TLSConfig{}
+	if v := os.Getenv(EnvNomadCaCert); v != "" {
+		tlsConfig.CACert = v
+	}
+
+	if v := os.Getenv(EnvNomadCaPath); v != "" {
+		tlsConfig.CAPath = v
+	}
+
+	if v := os.Getenv(EnvNomadClientCert); v != "" {
+		tlsConfig.ClientCert = v
+	}
+
+	if v := os.Getenv(EnvNomadClientKey); v != "" {
+		tlsConfig.ClientKey = v
+	}
+
+	if v := os.Getenv(EnvNomadTlsServerName); v != "" {
+		tlsConfig.TLSServerName = v
+	}
+
+	if v := os.Getenv(EnvNomadSkipVerify); v != "" {
+		skipVerify, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, err
+		}
+		if skipVerify {
+			tlsConfig.Insecure = true
+		}
+	}
+
+	return &tlsConfig, nil
 }
 
 func GetNomadLogPath(api *APIClient) (string, error) {
