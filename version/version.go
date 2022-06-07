ddde6b53d6ec587d20537c7a5fcddfe0675773cb
Nick Chandler <nwchandler@users.noreply.github.com>
2022-06-07 12:49:08 -0400

    backport crt changes

---
+++ version/version.go
@@ -0,0 +1,88 @@
+package version
+
+import (
+	"fmt"
+)
+
+const (
+	slug = "hcdiag v"
+)
+
+var (
+	// version is the main version number that is being run at the moment.
+	//
+	// version must be of the format <MAJOR>.<MINOR>.<PATCH>, as described in the semantic versioning specification.
+	version = "0.3.0"
+
+	// prerelease is a pre-release marker for the version. If this is "" (empty string) then it means that
+	// it is a final release. Otherwise, this is a pre-release such as "dev" (in development),
+	// "beta", "rc1", etc.
+	prerelease = "rc0"
+
+	// metadata is any additional (optional) information regarding the build, as described by the semantic
+	// versioning specification.
+	metadata string
+
+	// gitCommit is the commit associated with the build. It is set by the build process and should not require
+	// human updates under normal circumstances.
+	gitCommit string
+
+	// buildDate is the date/time when the build was created. It is set by the build process and should not require
+	// human updates under normal circumstances.
+	buildDate string
+)
+
+// Version is a container for version information.
+type Version struct {
+	Version    string `json:"version,omitempty"`
+	Prerelease string `json:"prerelease,omitempty"`
+	Metadata   string `json:"build_metadata,omitempty"`
+	Revision   string `json:"revision,omitempty"`
+	BuildDate  string `json:"build_date,omitempty"`
+}
+
+// GetVersion produces a Version that includes fields set based on version package variables.
+func GetVersion() Version {
+	return Version{
+		Version:    version,
+		Prerelease: prerelease,
+		Metadata:   metadata,
+		Revision:   gitCommit,
+		BuildDate:  buildDate,
+	}
+}
+
+// SemanticVersion produces a semantic version number from a Version object.
+func (v Version) SemanticVersion() string {
+	sv := v.Version
+
+	if v.Prerelease != "" {
+		sv = fmt.Sprintf("%s-%s", sv, v.Prerelease)
+	}
+
+	if v.Metadata != "" {
+		sv = fmt.Sprintf("%s+%s", sv, v.Metadata)
+	}
+
+	return sv
+}
+
+// FullVersionNumber produces a human-readable string representation of the Version object.
+//
+// In addition to a short slug that includes the product name (hcdiag) and the semantic version,
+// the Revision will be included if the optional argument, `rev`, is true. Further, if a buildDate
+// is set, it is also included in the output.
+func (v Version) FullVersionNumber(rev bool) string {
+	versionString := slug
+	versionString += v.SemanticVersion()
+
+	if rev && v.Revision != "" {
+		versionString += fmt.Sprintf(" (%s)", v.Revision)
+	}
+
+	if v.BuildDate != "" {
+		versionString += fmt.Sprintf(", built %s", v.BuildDate)
+	}
+
+	return versionString
+}
---
+++ version/version_test.go
@@ -0,0 +1,61 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetVersion(t *testing.T) {
+	vi := GetVersion()
+	assert.Equal(t, version, vi.Version)
+	assert.Equal(t, prerelease, vi.Prerelease)
+}
+
+func TestVersion_SemanticVersion(t *testing.T) {
+	testCases := []struct {
+		name     string
+		v        Version
+		expected string
+	}{
+		{
+			name: "Test only Version",
+			v: Version{
+				Version: "0.0.1",
+			},
+			expected: "0.0.1",
+		},
+		{
+			name: "Test Prerelease",
+			v: Version{
+				Version:    "0.0.1",
+				Prerelease: "test",
+			},
+			expected: "0.0.1-test",
+		},
+		{
+			name: "Test Metadata",
+			v: Version{
+				Version:  "0.0.1",
+				Metadata: "buildinfo",
+			},
+			expected: "0.0.1+buildinfo",
+		},
+		{
+			name: "Test All",
+			v: Version{
+				Version:    "0.0.1",
+				Prerelease: "test",
+				Metadata:   "buildinfo",
+			},
+			expected: "0.0.1-test+buildinfo",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			vn := tc.v.SemanticVersion()
+			assert.Equal(t, vn, tc.expected)
+		})
+	}
+}
