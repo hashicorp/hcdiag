48c8df566bb3d7a51bff23cfa884619ef3929864
Kit Patella <kit@jepsen.io>
2022-07-29 10:48:31 -0700

    add httper redactions, fix imports in commander

--- runner/httper.go
+++ runner/httper.go
@@ -1,6 +1,10 @@
 package runner
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
+
 	"github.com/hashicorp/hcdiag/client"
 	"github.com/hashicorp/hcdiag/op"
 	"github.com/hashicorp/hcdiag/redact"
@@ -27,8 +31,22 @@ func (h HTTPer) ID() string {
 // Run executes a GET request to the Path using the Client
 func (h HTTPer) Run() op.Op {
 	result, err := h.Client.Get(h.Path)
+	redResult, redErr := h.redact(fmt.Sprint(result))
 	if err != nil {
-		return op.New(h.ID(), result, op.Unknown, err, Params(h))
+		if redErr != nil {
+			return op.New(h.ID(), nil, op.Fail, redErr, Params(h))
+		}
+		return op.New(h.ID(), redResult, op.Unknown, err, Params(h))
 	}
-	return op.New(h.ID(), result, op.Success, nil, Params(h))
+	return op.New(h.ID(), redResult, op.Success, nil, Params(h))
+}
+
+func (h HTTPer) redact(result string) (string, error) {
+	r := strings.NewReader(result)
+	buf := new(bytes.Buffer)
+	err := redact.ApplyMany(h.Redactions, buf, r)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
