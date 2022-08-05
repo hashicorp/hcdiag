b806c733c6cf037f684320d2b1041a1503f8ba8f
David Cohen <dcohen@hashicorp.com>
2022-08-05 13:55:26 -0400

    add redaction flow for TFE and Vault products

--- product/tfe.go
+++ product/tfe.go
@@ -4,11 +4,15 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/hcdiag/client"
 	"github.com/hashicorp/hcdiag/hcl"
+	"github.com/hashicorp/hcdiag/redact"
 	"github.com/hashicorp/hcdiag/runner"
 )
 
 // NewTFE takes a logger and product config, and it creates a Product with all of TFE's default runners.
 func NewTFE(logger hclog.Logger, cfg Config) (*Product, error) {
+	// Prepend product-specific redactions to agent-level redactions from cfg
+	cfg.Redactions = redact.Flatten(getDefaultTFERedactions(), cfg.Redactions)
+
 	product := &Product{
 		l:      logger.Named("product"),
 		Name:   TFE,
@@ -19,12 +23,15 @@ func NewTFE(logger hclog.Logger, cfg Config) (*Product, error) {
 		return nil, err
 	}
 
-	product.Runners, err = tfeRunners(cfg, api)
-	if err != nil {
-		return nil, err
-	}
-
 	if cfg.HCL != nil {
+		// Map product-specific redactions from our config
+		hclProductRedactions, err := hcl.MapRedacts(cfg.HCL.Redactions)
+		if err != nil {
+			return nil, err
+		}
+		// Prepend product HCL redactions to our product defaults
+		cfg.Redactions = redact.Flatten(hclProductRedactions, cfg.Redactions)
+
 		hclRunners, err := hcl.BuildRunners(cfg.HCL, cfg.TmpDir, api, cfg.Since, cfg.Until, nil)
 		if err != nil {
 			return nil, err
@@ -34,22 +41,55 @@ func NewTFE(logger hclog.Logger, cfg Config) (*Product, error) {
 		product.Selects = cfg.HCL.Selects
 	}
 
+	// Add built-in runners
+	builtInRunners, err := tfeRunners(cfg, api)
+	if err != nil {
+		return nil, err
+	}
+	product.Runners = append(product.Runners, builtInRunners...)
+
 	return product, nil
 }
 
 // tfeRunners configures a set of default runners for TFE.
 func tfeRunners(cfg Config, api *client.APIClient) ([]runner.Runner, error) {
 	return []runner.Runner{
-		runner.NewCommander("replicatedctl support-bundle", "string", nil),
+		runner.NewCommander("replicatedctl support-bundle", "string", cfg.Redactions),
 
-		runner.NewCopier("/var/lib/replicated/support-bundles/replicated-support*.tar.gz", cfg.TmpDir, cfg.Since, cfg.Until, nil),
+		runner.NewCopier("/var/lib/replicated/support-bundles/replicated-support*.tar.gz", cfg.TmpDir, cfg.Since, cfg.Until, cfg.Redactions),
 
-		runner.NewHTTPer(api, "/api/v2/admin/customization-settings", nil),
-		runner.NewHTTPer(api, "/api/v2/admin/general-settings", nil),
-		runner.NewHTTPer(api, "/api/v2/admin/organizations", nil),
-		runner.NewHTTPer(api, "/api/v2/admin/terraform-versions", nil),
-		runner.NewHTTPer(api, "/api/v2/admin/twilio-settings", nil),
+		runner.NewHTTPer(api, "/api/v2/admin/customization-settings", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/general-settings", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/organizations", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/terraform-versions", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/twilio-settings", cfg.Redactions),
 		// page size 1 because we only actually care about total workspace count in the `meta` field
-		runner.NewHTTPer(api, "/api/v2/admin/workspaces?page[size]=1", nil),
+		runner.NewHTTPer(api, "/api/v2/admin/workspaces?page[size]=1", cfg.Redactions),
 	}, nil
+}
+
+// getDefaultTFERedactions returns a slice of default redactions for this product
+func getDefaultTFERedactions() []*redact.Redact {
+	redactions := []struct {
+		name    string
+		matcher string
+		replace string
+	}{
+		{
+			name:    "TFE-product-default",
+			matcher: "/tfe/",
+			replace: "TFE-product-default-redaction",
+		},
+	}
+
+	var defaultTFERedactions = make([]*redact.Redact, len(redactions))
+	for i, r := range redactions {
+		redaction, err := redact.New(r.matcher, "", r.replace)
+		if err != nil {
+			// If there's an issue, return an empty slice so that we can just ignore these redactions
+			return make([]*redact.Redact, 0)
+		}
+		defaultTFERedactions[i] = redaction
+	}
+	return defaultTFERedactions
 }
--- product/vault.go
+++ product/vault.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/hashicorp/hcdiag/hcl"
+	"github.com/hashicorp/hcdiag/redact"
 
 	"github.com/hashicorp/go-hclog"
 
@@ -21,6 +22,9 @@ const (
 
 // NewVault takes a product config and creates a Product containing all of Vault's runners.
 func NewVault(logger hclog.Logger, cfg Config) (*Product, error) {
+	// Prepend product-specific redactions to agent-level redactions from cfg
+	cfg.Redactions = redact.Flatten(getDefaultVaultRedactions(), cfg.Redactions)
+
 	product := &Product{
 		l:      logger.Named("product"),
 		Name:   Vault,
@@ -31,11 +35,15 @@ func NewVault(logger hclog.Logger, cfg Config) (*Product, error) {
 		return nil, err
 	}
 
-	product.Runners, err = vaultRunners(cfg, api)
-	if err != nil {
-		return nil, err
-	}
 	if cfg.HCL != nil {
+		// Map product-specific redactions from our config
+		hclProductRedactions, err := hcl.MapRedacts(cfg.HCL.Redactions)
+		if err != nil {
+			return nil, err
+		}
+		// Prepend product HCL redactions to our product defaults
+		cfg.Redactions = redact.Flatten(hclProductRedactions, cfg.Redactions)
+
 		hclRunners, err := hcl.BuildRunners(cfg.HCL, cfg.TmpDir, api, cfg.Since, cfg.Until, nil)
 		if err != nil {
 			return nil, err
@@ -45,18 +53,25 @@ func NewVault(logger hclog.Logger, cfg Config) (*Product, error) {
 		product.Selects = cfg.HCL.Selects
 	}
 
+	// Add built-in runners
+	builtInRunners, err := vaultRunners(cfg, api)
+	if err != nil {
+		return nil, err
+	}
+	product.Runners = append(product.Runners, builtInRunners...)
+
 	return product, nil
 }
 
 // vaultRunners provides a list of default runners to inspect vault.
 func vaultRunners(cfg Config, api *client.APIClient) ([]runner.Runner, error) {
 	runners := []runner.Runner{
-		runner.NewCommander("vault version", "string", nil),
-		runner.NewCommander("vault status -format=json", "json", nil),
-		runner.NewCommander("vault read sys/health -format=json", "json", nil),
-		runner.NewCommander("vault read sys/seal-status -format=json", "json", nil),
-		runner.NewCommander("vault read sys/host-info -format=json", "json", nil),
-		runner.NewCommander(fmt.Sprintf("vault debug -output=%s/VaultDebug.tar.gz -duration=%s -interval=%s", cfg.TmpDir, cfg.DebugDuration, cfg.DebugInterval), "string", nil),
+		runner.NewCommander("vault version", "string", cfg.Redactions),
+		runner.NewCommander("vault status -format=json", "json", cfg.Redactions),
+		runner.NewCommander("vault read sys/health -format=json", "json", cfg.Redactions),
+		runner.NewCommander("vault read sys/seal-status -format=json", "json", cfg.Redactions),
+		runner.NewCommander("vault read sys/host-info -format=json", "json", cfg.Redactions),
+		runner.NewCommander(fmt.Sprintf("vault debug -output=%s/VaultDebug.tar.gz -duration=%s -interval=%s", cfg.TmpDir, cfg.DebugDuration, cfg.DebugInterval), "string", cfg.Redactions),
 
 		logs.NewDocker("vault", cfg.TmpDir, cfg.Since),
 		logs.NewJournald("vault", cfg.TmpDir, cfg.Since, cfg.Until),
@@ -65,9 +80,35 @@ func vaultRunners(cfg Config, api *client.APIClient) ([]runner.Runner, error) {
 	// try to detect log location to copy
 	if logPath, err := client.GetVaultAuditLogPath(api); err == nil {
 		dest := filepath.Join(cfg.TmpDir, "logs/vault")
-		logCopier := runner.NewCopier(logPath, dest, cfg.Since, cfg.Until, nil)
+		logCopier := runner.NewCopier(logPath, dest, cfg.Since, cfg.Until, cfg.Redactions)
 		runners = append([]runner.Runner{logCopier}, runners...)
 	}
 
 	return runners, nil
+}
+
+// getDefaultVaultRedactions returns a slice of default redactions for this product
+func getDefaultVaultRedactions() []*redact.Redact {
+	redactions := []struct {
+		name    string
+		matcher string
+		replace string
+	}{
+		{
+			name:    "Vault-product-default",
+			matcher: "/vault/",
+			replace: "Vault-product-default-redaction",
+		},
+	}
+
+	var defaultVaultRedactions = make([]*redact.Redact, len(redactions))
+	for i, r := range redactions {
+		redaction, err := redact.New(r.matcher, "", r.replace)
+		if err != nil {
+			// If there's an issue, return an empty slice so that we can just ignore these redactions
+			return make([]*redact.Redact, 0)
+		}
+		defaultVaultRedactions[i] = redaction
+	}
+	return defaultVaultRedactions
 }
