1869c027461e5826ea998f75604ab4b10c5de0d6
anniehedgpeth <annie@hashicorp.com>
2022-09-09 15:15:50 -0600

    Adding more data collection for TFE

--- product/tfe.go
+++ product/tfe.go
@@ -69,10 +69,18 @@ func tfeRunners(cfg Config, api *client.APIClient) ([]runner.Runner, error) {
 		runner.NewHTTPer(api, "/api/v2/admin/twilio-settings", cfg.Redactions),
 		// page size 1 because we only actually care about total workspace count in the `meta` field
 		runner.NewHTTPer(api, "/api/v2/admin/workspaces?page[size]=1", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/users?page[size]=1", cfg.Redactions),
+		runner.NewHTTPer(api, "/api/v2/admin/runs?page[size]=1", cfg.Redactions),
 
 		runner.NewCommander("docker -v", "string", cfg.Redactions),
 		runner.NewCommander("replicatedctl app status --output json", "json", cfg.Redactions),
 		runner.NewCommander("lsblk --json", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl app-config view -o json --group capacity", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl app-config view -o json --group production_type", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl app-config view -o json --group log_forwarding", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl app-config view -o json --group blob", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl app-config view -o json --group worker_image", "json", cfg.Redactions),
+		runner.NewCommander("replicatedctl params export --template '{{.Airgap}}'", "string", cfg.Redactions),
 
 		runner.NewSheller("getenforce", cfg.Redactions),
 		runner.NewSheller("env | grep -i proxy", cfg.Redactions),
