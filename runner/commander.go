54beee79348e86a2242e277706df174e6faa6ef1
Nick Chandler <nwchandler@users.noreply.github.com>
2022-10-03 17:41:47 -0400

    Improve Handling of Commander Args with Spaces (#239)
    This intends to improve hcdiag's ability to handle commander args that include spaces in them on Linux/Unix OS's. Previously, such arguments would be inappropriately split, even if they were wrapped within single quotes, which caused the command to be unable to make sense of the arguments. A library has been included which should alleviate this issue, and tests were added around the capability.
    
    Note that no changes are made to Windows command argument processing. The underlying mechanism for Windows command calls is done differently than *nix, so the existing behavior worked for Windows.

--- runner/commander.go
+++ runner/commander.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os/exec"
+	"runtime"
 	"strings"
 
+	"github.com/cosiner/argv"
 	"github.com/hashicorp/hcdiag/op"
 	"github.com/hashicorp/hcdiag/redact"
 	"github.com/hashicorp/hcdiag/util"
@@ -35,18 +37,19 @@ func (c Commander) ID() string {
 
 // Run executes the Command
 func (c Commander) Run() op.Op {
-	bits := strings.Split(c.Command, " ")
-	cmd := bits[0]
-	args := bits[1:]
+	p, err := parseCommand(c.Command)
+	if err != nil {
+		return op.New(c.ID(), nil, op.Fail, err, Params(c))
+	}
 
 	// Exit early with a wrapped error if the command isn't found on this system
-	_, err := util.HostCommandExists(cmd)
+	_, err = util.HostCommandExists(p.cmd)
 	if err != nil {
 		return op.New(c.ID(), nil, op.Skip, err, Params(c))
 	}
 
 	// Execute command
-	bts, err := exec.Command(cmd, args...).CombinedOutput()
+	bts, err := exec.Command(p.cmd, p.args...).CombinedOutput()
 	if err != nil {
 		err1 := CommandExecError{command: c.Command, format: c.Format, err: err}
 		return op.New(c.ID(), string(bts), op.Unknown, err1, Params(c))
@@ -96,6 +99,72 @@ func (c Commander) Run() op.Op {
 	}
 }
 
+type parsedCommand struct {
+	cmd  string
+	args []string
+	err  error
+}
+
+func parseCommand(command string) (parsedCommand, error) {
+	parsed := parsedCommand{}
+
+	// Under the hood, the arguments provided to Windows are re-joined into a single string, allowing the Windows
+	// OS to handle splitting however it needs to (see syscall/exec_windows.go, along with doc comments for exec.Command).
+	// Allowing Windows paths through to argv can cause issues with character escaping; rather than adding complexity
+	// to command parsing, just to have the args rejoined later on Windows, we short-circuit here, pulling out the cmd
+	// name and arguments using a simple split on spaces.
+	if runtime.GOOS == "windows" {
+		split := strings.Split(command, " ")
+		parsed.cmd = split[0]
+		parsed.args = split[1:]
+		return parsed, nil
+	}
+
+	// Argv returns a [][]string, where each outer slice represents commands split by '|' and the inner slices
+	// have the command at element 0 and any arguments to the command in the remaining elements.
+	p, err := argv.Argv(command, nil, nil)
+	if err != nil {
+		e := CommandParseError{
+			command: command,
+			err:     err,
+		}
+		parsed.err = e
+		return parsed, e
+	}
+
+	// We only support a single command, without piping from one to the next, in Commander
+	if len(p) > 1 {
+		e := CommandParseError{
+			command: command,
+			err:     fmt.Errorf("piped commands are unsupported, please use a Sheller runner or multiple Commander runners, command=%s", command),
+		}
+		parsed.err = e
+		return parsed, e
+	}
+
+	parsed.cmd = p[0][0]
+	parsed.args = p[0][1:]
+
+	return parsed, nil
+}
+
+var _ error = CommandParseError{}
+
+type CommandParseError struct {
+	command string
+	err     error
+}
+
+func (e CommandParseError) Error() string {
+	return fmt.Sprintf("error parsing command in Commander runner, command=%s, error=%s", e.command, e.err.Error())
+}
+
+func (e CommandParseError) Unwrap() error {
+	return e.err
+}
+
+var _ error = CommandExecError{}
+
 type CommandExecError struct {
 	command string
 	format  string
@@ -110,6 +179,8 @@ func (e CommandExecError) Unwrap() error {
 	return e.err
 }
 
+var _ error = UnmarshalError{}
+
 type UnmarshalError struct {
 	command string
 	err     error
@@ -122,6 +193,8 @@ func (e UnmarshalError) Error() string {
 func (e UnmarshalError) Unwrap() error {
 	return e.err
 }
+
+var _ error = FormatUnknownError{}
 
 type FormatUnknownError struct {
 	command string
