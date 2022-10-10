ad0c750f9e8e9242444e32b98fe2d5a1e55ec942
Dave Cohen <1267040+groovemonkey@users.noreply.github.com>
2022-10-10 16:19:19 -0400

    Count nested operations in StatusCounts (#245)
    * add op.WalkStatuses to recursively walk the tree of result ops
    * Co-authored by @mkcp and @nwchandler

--- op/op.go
+++ op/op.go
@@ -57,12 +57,43 @@ func New(id string, result map[string]any, status Status, err error, params map[
 
 // StatusCounts takes a slice of op references and returns a map containing sums of each Status
 func StatusCounts(ops map[string]Op) (map[Status]int, error) {
-	statuses := make(map[Status]int)
-	for _, o := range ops {
-		if o.Status == "" {
-			return nil, fmt.Errorf("unable to build Statuses map, op not run: op=%s", o.Identifier)
-		}
-		statuses[o.Status]++
+	// copy our input into a new map that conforms to our Walk input type
+	m := make(map[string]any, len(ops))
+	for k, v := range ops {
+		m[k] = v
+	}
+
+	statuses := WalkStatuses(m)
+
+	if val, ok := statuses[""]; ok {
+		return nil, fmt.Errorf("op.StatusCounts received ops that did not have a status, count=%d", val)
 	}
 	return statuses, nil
+}
+
+// WalkStatuses performs a depth-first search of a tree of results and returns a map of status counts
+func WalkStatuses(results map[string]any) map[Status]int {
+	statuses := make(map[Status]int)
+
+	var walkStatuses func(map[Status]int, map[string]any)
+	walkStatuses = func(accumulator map[Status]int, results map[string]any) {
+		for _, res := range results {
+			switch res := res.(type) {
+
+			case map[string]any:
+				walkStatuses(statuses, res)
+
+			case Op:
+				// Increment and recur
+				statuses[res.Status]++
+				walkStatuses(statuses, res.Result)
+
+			// End of branch reached
+			default:
+				continue
+			}
+		}
+	}
+	walkStatuses(statuses, results)
+	return statuses
 }
