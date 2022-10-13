hashicorp/hcdiag

commit 96c43aa048d5823c7672b5c650d23cf4f91e2392

Incorporate mitchellh/cli Library into hcdiag (#222)
This is the initial inclusion of the mitchellh/cli library into hcdiag. Going alongside this, it also makes some related changes that intend to smooth the process of adding additional subcommands in the future.

The main.go file is now primarily used to set up and execute the CLI. Flag parsing and agent instantiation have moved into `commandd/run.go`.

Executing hcdiag without any subcommands will execute a diagnostic run, matching current (0.4.0) behavior, however the `run` subcommand is preferred. The help text for the application indicates that this behavior will change in the future, and users should begin to use the `run` subcommand.

The version display has become its own subcommand. However, the `--version` flag will also still work. This behavior is more consistent with other HashiCorp products, which commonly have their own `version` subcommand.

command/run.go
@@ -1,0 +1,308 @@
+package command
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/mitchellh/cli"
+
+	"github.com/hashicorp/hcdiag/agent"
+	"github.com/hashicorp/hcdiag/hcl"
+	"github.com/hashicorp/hcdiag/product"
+)
+
+// seventyTwoHours represents the duration "72h" parsed in nanoseconds
+const seventyTwoHours = 72 * time.Hour
+
+var _ cli.Command = &RunCommand{}
+
+type RunCommand struct {
+	ui    cli.Ui
+	flags *flag.FlagSet
+
+	os     string
+	serial bool
+	dryrun bool
+
+	// Products
+	autoDetectProducts bool
+	consul             bool
+	nomad              bool
+	tfe                bool
+	vault              bool
+
+	// since provides a time range for ops to work from
+	since time.Duration
+
+	// includeSince provides a time range for ops to work from
+	includeSince time.Duration
+
+	// includes
+	includes []string
+
+	// Bundle write location
+	destination string
+
+	// HCL file location
+	config string
+
+	// debugDuration param for product debug bundles
+	debugDuration time.Duration
+
+	// debugInterval param for product debug bundles
+	debugInterval time.Duration
+}
+
+func (c *RunCommand) init() {
+	const (
+		consulUsageText        = "Run Consul diagnostics"
+		nomadUsageText         = "Run Nomad diagnostics"
+		terraformEntUsageText  = "Run Terraform Enterprise diagnostics"
+		vaultUsageText         = "Run Vault diagnostics"
+		autodetectUsageText    = "Auto-Detect installed products; any provided product flags will override this setting"
+		dryrunUsageText        = "Displays all runners that would be executed during a normal run without actually executing them."
+		serialUsageText        = "Run products in sequence rather than concurrently"
+		includeSinceUsageText  = "Alias for -since, will be overridden if -since is also provided, usage examples: '72h', '25m', '45s', '120h1m90s'"
+		sinceUsageText         = "Collect information within this time. Takes a 'go-formatted' duration, usage examples: '72h', '25m', '45s', '120h1m90s'"
+		debugDurationUsageText = "How long to run product debug bundle commands. Provide a duration ex: '00h00m00s'. See: -duration in 'vault debug', 'consul debug', and 'nomad operator debug'"
+		debugIntervalUsageText = "How long metrics collection intervals in product debug commands last. Provide a duration ex: '00h00m00s'. See: -interval in 'vault debug', 'consul debug', and 'nomad operator debug'"
+		osUsageText            = "Override operating system detection"
+		destinationUsageText   = "Path to the directory the bundle should be written in"
+		destUsageText          = "Shorthand for -destination"
+		configUsageText        = "Path to HCL configuration file"
+		includesUsageText      = "Files or directories to include (comma-separated, file-*-globbing available if 'wrapped-*-in-single-quotes'); e.g. '/var/log/consul-*,/var/log/nomad-*'"
+	)
+
+	// flag.ContinueOnError allows flag.Parse to return an error if one comes up, rather than doing an `os.Exit(2)`
+	// on its own.
+	c.flags = flag.NewFlagSet("run", flag.ContinueOnError)
+
+	c.flags.BoolVar(&c.dryrun, "dryrun", false, dryrunUsageText)
+	c.flags.BoolVar(&c.serial, "serial", false, serialUsageText)
+	c.flags.BoolVar(&c.consul, "consul", false, consulUsageText)
+	c.flags.BoolVar(&c.nomad, "nomad", false, nomadUsageText)
+	c.flags.BoolVar(&c.tfe, "terraform-ent", false, terraformEntUsageText)
+	c.flags.BoolVar(&c.vault, "vault", false, vaultUsageText)
+	c.flags.BoolVar(&c.autoDetectProducts, "autodetect", true, autodetectUsageText)
+	c.flags.DurationVar(&c.includeSince, "include-since", seventyTwoHours, includeSinceUsageText)
+	c.flags.DurationVar(&c.since, "since", seventyTwoHours, sinceUsageText)
+	c.flags.DurationVar(&c.debugDuration, "debug-duration", product.DefaultDuration, debugDurationUsageText)
+	c.flags.DurationVar(&c.debugInterval, "debug-interval", product.DefaultInterval, debugIntervalUsageText)
+	c.flags.StringVar(&c.os, "os", "auto", osUsageText)
+	c.flags.StringVar(&c.destination, "destination", ".", destinationUsageText)
+	c.flags.StringVar(&c.destination, "dest", ".", destUsageText)
+	c.flags.StringVar(&c.config, "config", "", configUsageText)
+	c.flags.Var(&CSVFlag{&c.includes}, "includes", includesUsageText)
+
+	// Ensure f.Destination points to some kind of directory by its notation
+	// FIXME(mkcp): trailing slashes should be trimmed in path.Dir... why does a double slash end in a slash?
+	c.destination = path.Dir(c.destination)
+
+	// When invalid flags are provided, Go will output a usage message of its own. If we direct our flag set to
+	// io.Discard, it will effectively be hidden, allowing us to print our own Help message upon failure.
+	c.flags.SetOutput(io.Discard)
+}
+
+// NewRunCommand produces a new *command pointer, initialized for use in a CLI application.
+func NewRunCommand(ui cli.Ui) *RunCommand {
+	c := &RunCommand{ui: ui}
+	c.init()
+	return c
+}
+
+// RunCommandFactory provides a cli.CommandFactory that will produce an appropriately-initiated *command.
+func RunCommandFactory(ui cli.Ui) cli.CommandFactory {
+	return func() (cli.Command, error) {
+		return NewRunCommand(ui), nil
+	}
+}
+
+// Help provides help text to users who pass in the --help flag or who enter invalid options.
+func (c *RunCommand) Help() string {
+	helpText := `Usage: hcdiag run [options]
+
+Executes an hcdiag diagnostics run on a local machine. Options are available to customize the execution.
+`
+
+	return Usage(helpText, c.flags)
+}
+
+// Synopsis provides a brief description of the command, for inclusion in the application's primary --help.
+func (c *RunCommand) Synopsis() string {
+	return "Execute an hcdiag diagnostic run"
+}
+
+// Run executes the command.
+func (c *RunCommand) Run(args []string) int {
+	if err := c.parseFlags(args); err != nil {
+		// Output the specific error to help the user understand what went wrong.
+		c.ui.Warn(err.Error())
+		// Since there was an issue in input, let's show our Help to try and assist the user.
+		c.ui.Warn(c.Help())
+		return FlagParseError
+	}
+
+	l := configureLogging("hcdiag")
+
+	// Build agent configuration from flags, HCL, and system time
+	var config agent.Config
+	// Parse and store HCL struct on agent.
+	if c.config != "" {
+		hclCfg, err := hcl.Parse(c.config)
+		if err != nil {
+			l.Error("Failed to load configuration", "config", c.config, "error", err)
+			return ConfigError
+		}
+		l.Debug("HCL config is", "hcl", hclCfg)
+		config.HCL = hclCfg
+	}
+	// Assign flag values to our agent.Config
+	cfg := c.mergeAgentConfig(config)
+
+	// Set config timestamps based on durations
+	now := time.Now()
+	since := pickSinceVsIncludeSince(l, c.since, c.includeSince)
+	cfg = setTime(cfg, now, since)
+	l.Debug("merged cfg", "cfg", fmt.Sprintf("%+v", cfg))
+
+	// Create agent
+	a, err := agent.NewAgent(cfg, l)
+	if err != nil {
+		l.Error("problem creating agent", err)
+		return AgentSetupError
+	}
+
+	// Run the agent
+	errs := a.Run()
+	if 0 < len(errs) {
+		return AgentExecutionError
+	}
+
+	// TODO (nwchandler): Pass in the client UI for output
+	if err = a.WriteSummary(os.Stdout); err != nil {
+		l.Warn("failed to generate report summary; please review output files to ensure everything expected is present", "err", err)
+		return AgentExecutionError
+	}
+
+	return Success
+}
+
+// configureLogging takes a logger name, sets the default configuration, grabs the LOG_LEVEL from our ENV vars, and
+//  returns a configured and usable logger.
+func configureLogging(loggerName string) hclog.Logger {
+	// Create logger, set default and log level
+	appLogger := hclog.New(&hclog.LoggerOptions{
+		Name:  loggerName,
+		Color: hclog.AutoColor,
+	})
+	hclog.SetDefault(appLogger)
+	if logStr := os.Getenv("LOG_LEVEL"); logStr != "" {
+		if level := hclog.LevelFromString(logStr); level != hclog.NoLevel {
+			appLogger.SetLevel(level)
+			appLogger.Debug("Logger configuration change", "LOG_LEVEL", hclog.Fmt("%s", logStr))
+		}
+	}
+	return hclog.Default()
+}
+
+type CSVFlag struct {
+	Values *[]string
+}
+
+func (s CSVFlag) String() string {
+	if s.Values == nil {
+		return ""
+	}
+	return strings.Join(*s.Values, ",")
+}
+
+func (s CSVFlag) Set(v string) error {
+	*s.Values = strings.Split(v, ",")
+	return nil
+}
+
+func (c *RunCommand) parseFlags(args []string) error {
+	return c.flags.Parse(args)
+}
+
+// mergeAgentConfig merges flags into the agent.Config, prioritizing flags over HCL config.
+func (c *RunCommand) mergeAgentConfig(config agent.Config) agent.Config {
+	config.OS = c.os
+	config.Serial = c.serial
+	config.Dryrun = c.dryrun
+
+	config.Consul = c.consul
+	config.Nomad = c.nomad
+	config.TFE = c.tfe
+	config.Vault = c.vault
+
+	// If any products have been set manually, then we do not care about product auto-detection
+	if c.autoDetectProducts && !checkProductsSet(config) {
+		config.Consul = autoDetectCommand("consul")
+		config.Nomad = autoDetectCommand("nomad")
+		config.TFE = autoDetectCommand("terraform")
+		config.Vault = autoDetectCommand("vault")
+
+		if checkProductsSet(config) {
+			hclog.L().Info(
+				"Auto-detected products; if you wish to limit hcdiag, please use the appropriate -<product> flag and run again",
+				"consul", config.Consul,
+				"nomad", config.Nomad,
+				"terraform", config.TFE,
+				"vault", config.Vault,
+			)
+		}
+	}
+
+	// Params for --includes
+	config.Includes = c.includes
+
+	// Bundle write location
+	config.Destination = c.destination
+
+	// Apply Debug{Duration,Interval}
+	config.DebugDuration = c.debugDuration
+	config.DebugInterval = c.debugInterval
+
+	return config
+}
+
+// checkProductsSet returns true if any of the individual products are true in the provided config
+func checkProductsSet(config agent.Config) bool {
+	return config.Consul || config.Nomad || config.TFE || config.Vault
+}
+
+func autoDetectCommand(cmd string) bool {
+	p, err := exec.LookPath(cmd)
+	if err != nil {
+		return false
+	}
+	hclog.L().Debug("Found command", "cmd", cmd, "path", p)
+	return true
+}
+
+// pickSinceVsIncludeSince if Since is default and IncludeSince is NOT default, use IncludeSince
+func pickSinceVsIncludeSince(l hclog.Logger, since, includeSince time.Duration) time.Duration {
+	if since == seventyTwoHours && includeSince != seventyTwoHours {
+		l.Debug("includeSince set and default since", "includeSince", includeSince)
+		return includeSince
+	}
+	return since
+}
+
+func setTime(cfg agent.Config, now time.Time, since time.Duration) agent.Config {
+	// Capture a now value and set timestamps based on the same Now value
+	// Get the difference between now and the provided --since Duration
+	cfg.Since = now.Add(-since)
+	// NOTE(mkcp): In the future, cfg.Until may be set by a flag.
+	cfg.Until = time.Time{}
+
+	return cfg
+}

agent/agent.go
@@ -179,11 +179,6 @@ func (a *Agent) Run() []error {
 		a.l.Error("Failed to cleanup after the run", "error", errCleanup)
 	}
 
-	a.l.Info("Writing summary of products and ops to standard output")
-	if errSummary := a.WriteSummary(os.Stdout); errSummary != nil {
-		errs = append(errs, errSummary)
-		a.l.Error("Failed to write summary report following run", "error", errSummary)
-	}
 	return errs
 }
 

command/return_codes.go
@@ -1,0 +1,22 @@
+package command
+
+// Success indicates a successful command execution.
+const Success int = 0
+
+// The following error group is intended for issues within the initial setup process of a command's execution.
+const (
+	// FlagParseError indicates that a command was unable to successfully parse the flags/arguments provided to it.
+	FlagParseError int = iota + 16
+
+	// ConfigError indicates that there was an error in the hcdiag configuration.
+	ConfigError
+)
+
+// The following error group is intended for issues with the Agent.
+const (
+	// AgentSetupError is returned when the agent cannot be setup properly prior to command execution.
+	AgentSetupError int = iota + 32
+
+	// AgentExecutionError is returned when the agent returns an error to the calling command.
+	AgentExecutionError
+)

command/run_test.go
@@ -1,11 +1,10 @@
-package main
+package command
 
 import (
 	"testing"
 	"time"
 
 	"github.com/hashicorp/hcdiag/agent"
-
 	"github.com/stretchr/testify/assert"
 )
 

command/usage.go
@@ -1,0 +1,70 @@
+package command
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/kr/text"
+)
+
+// maxLineLength is the maximum width of any line.
+const maxLineLength int = 72
+
+func Usage(txt string, flags *flag.FlagSet) string {
+	u := &Usager{
+		Usage: txt,
+		Flags: flags,
+	}
+	return u.String()
+}
+
+type Usager struct {
+	Usage string
+	Flags *flag.FlagSet
+}
+
+func (u *Usager) String() string {
+	out := new(bytes.Buffer)
+
+	// Write out the usage slug.
+	out.WriteString(strings.TrimSpace(u.Usage))
+	out.WriteString("\n")
+	out.WriteString("\n")
+
+	if u.Flags != nil {
+		printTitle(out, "Command Options")
+
+		u.Flags.VisitAll(func(f *flag.Flag) {
+			printFlag(out, f)
+		})
+	}
+
+	return strings.TrimRight(out.String(), "\n")
+}
+
+// printTitle prints a consistently-formatted title to the given writer.
+func printTitle(w io.Writer, s string) {
+	_, _ = fmt.Fprintf(w, "%s\n\n", s)
+}
+
+// printFlag prints a single flag to the given writer.
+func printFlag(w io.Writer, f *flag.Flag) {
+	_, _ = fmt.Fprintf(w, "  -%s\n", f.Name)
+
+	indented := wrapAtLength(f.Usage, 5)
+	_, _ = fmt.Fprintf(w, "%s\n\n", indented)
+}
+
+// wrapAtLength wraps the given text at the maxLineLength, taking into account
+// any provided left padding.
+func wrapAtLength(s string, pad int) string {
+	wrapped := text.Wrap(s, maxLineLength-pad)
+	lines := strings.Split(wrapped, "\n")
+	for i, line := range lines {
+		lines[i] = strings.Repeat(" ", pad) + line
+	}
+	return strings.Join(lines, "\n")
+}

command/version.go
@@ -1,0 +1,38 @@
+package command
+
+import (
+	"github.com/hashicorp/hcdiag/version"
+	"github.com/mitchellh/cli"
+)
+
+var _ cli.Command = &VersionCommand{}
+
+type VersionCommand struct {
+	ui cli.Ui
+}
+
+func NewVersionCommand(ui cli.Ui) *VersionCommand {
+	return &VersionCommand{ui: ui}
+}
+
+// VersionCommandFactory provides a cli.CommandFactory that will produce an appropriately-initiated *command.
+func VersionCommandFactory(ui cli.Ui) cli.CommandFactory {
+	return func() (cli.Command, error) {
+		return NewVersionCommand(ui), nil
+	}
+}
+
+func (c VersionCommand) Help() string {
+	return "Usage: hcdiag version"
+}
+
+func (c VersionCommand) Run([]string) int {
+	v := version.GetVersion()
+	c.ui.Output(v.FullVersionNumber(true))
+
+	return Success
+}
+
+func (c VersionCommand) Synopsis() string {
+	return "Print the current version of hcdiag"
+}

go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/hashicorp/go-hclog v0.16.2
 	github.com/hashicorp/go-rootcerts v1.0.2
 	github.com/hashicorp/hcl/v2 v2.11.1
+	github.com/kr/text v0.2.0
+	github.com/mitchellh/cli v1.1.4
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/shirou/gopsutil/v3 v3.22.5
@@ -13,17 +15,33 @@ require (
 )
 
 require (
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.0 // indirect
+	github.com/armon/go-radix v0.0.0-20180808171621-7fddfc383310 // indirect
+	github.com/bgentry/speakeasy v0.1.0 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/frankban/quicktest v1.14.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/nwaples/rardecode v1.1.3 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	github.com/posener/complete v1.1.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 )
 
 require (

commit 88753a6eb722be341597dec2d49229026950e99d

Skip Displaying Run Summary on Dry Runs (#252)
This merge addresses a UX bug introduced in #222 . With the changes introduced there, we began displaying a summary report at the end of dry runs, however the output was nonsensical because no runners had executed, and no results were written to disk. This change puts some condition checking in place to ensure we do not write the output summary when a user does a dry run.

command/run.go
@@ -189,6 +189,11 @@ func (c *RunCommand) Run(args []string) int {
 		return AgentExecutionError
 	}
 
+	// Skip any post-processing/reporting on dry runs because there are no results to handle
+	if c.dryrun {
+		return Success
+	}
+
 	resultsFile := a.ResultsDest()
 	if err = writeSummary(os.Stdout, resultsFile, a.ManifestOps); err != nil {
 		l.Warn("failed to generate report summary; please review output files to ensure everything expected is present", "err", err)