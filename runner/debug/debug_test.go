cb8aa40c1fde9746684c4a05f9e212fae99f8fdf
David Cohen <dcohen@hashicorp.com>
2022-11-01 17:49:46 -0400

    first crack at a simple (wrapped) debug runner for Nomad, Vault, and Consul. Also some tests

---
+++ runner/debug/debug_test.go
@@ -0,0 +1,184 @@
+package debug
+
+import (
+	"testing"
+	"time"
+
+	"github.com/hashicorp/hcdiag/product"
+	"github.com/hashicorp/hcdiag/redact"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleDebug(t *testing.T) {
+	tcs := []struct {
+		name    string
+		cfg     product.Config
+		filters []string
+		expect  string
+	}{
+		{
+			name: "lower-cased filter values should produce correctly-cased options",
+			cfg: product.Config{
+				Name:          "nomad",
+				TmpDir:        "/tmp/hcdiag",
+				DebugDuration: 2 * time.Minute,
+				DebugInterval: 30 * time.Second,
+			},
+			filters: []string{"allocation", "job"},
+			expect:  "nomad operator debug -log-level=TRACE -duration=2m0s -interval=30s -node-id=all -max-nodes=100 -output=/tmp/hcdiag/ -event-topic=Allocation -event-topic=Job",
+		},
+		{
+			name: "a small vault config should produce the correct vault debug command",
+			cfg: product.Config{
+				Name:          "vault",
+				TmpDir:        "/tmp/hcdiag",
+				DebugDuration: 2 * time.Minute,
+				DebugInterval: 30 * time.Second,
+			},
+			filters: []string{"metrics", "pprof", "replication-status"},
+			expect:  "vault debug -compress=true -duration=2m0s -interval=30s -output=/tmp/hcdiag/VaultDebug.tar.gz -target=metrics -target=pprof -target=replication-status",
+		},
+		{
+			name: "a small consul config should produce the correct consul debug command",
+			cfg: product.Config{
+				Name:          "consul",
+				TmpDir:        "/tmp/hcdiag",
+				DebugDuration: 2 * time.Minute,
+				DebugInterval: 30 * time.Second,
+			},
+			filters: []string{"members", "metrics"},
+			expect:  "consul debug -duration=2m0s -interval=30s -output=/tmp/hcdiag/ConsulDebug -capture=members -capture=metrics",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			d := NewSimpleDebug(tc.cfg, tc.filters, []*redact.Redact{})
+			cmdString := d.Command.Command
+
+			if tc.expect != cmdString {
+				t.Error(tc.name, cmdString)
+			}
+		})
+	}
+}
+
+func TestProductFilterString(t *testing.T) {
+	tcs := []struct {
+		name      string
+		product   product.Name
+		filters   []string
+		expect    string
+		expectErr bool
+	}{
+		{
+			name:      "empty filters should return empty string 1",
+			product:   "nomad",
+			filters:   []string{""},
+			expect:    "",
+			expectErr: false,
+		},
+		{
+			name:      "empty filters should return empty string 2",
+			product:   "vault",
+			filters:   []string{},
+			expect:    "",
+			expectErr: false,
+		},
+		{
+			name:      "empty filters inside valid ones should be ignored",
+			product:   "consul",
+			filters:   []string{"members", "", "logs"},
+			expect:    " -capture=members -capture=logs",
+			expectErr: false,
+		},
+		{
+			name:      "test a valid nomad filter",
+			product:   "nomad",
+			filters:   []string{"ACLToken"},
+			expect:    " -event-topic=ACLToken",
+			expectErr: false,
+		},
+		{
+			name:      "test valid nomad filters",
+			product:   "nomad",
+			filters:   []string{"ACLToken", "ACLPolicy", "ACLRole", "Job"},
+			expect:    " -event-topic=ACLToken -event-topic=ACLPolicy -event-topic=ACLRole -event-topic=Job",
+			expectErr: false,
+		},
+		{
+			name:      "test an invalid consul filter",
+			product:   "consul",
+			filters:   []string{"ACLToken"},
+			expect:    "",
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := productFilterString(tc.product, tc.filters)
+
+			if tc.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expect, result)
+			}
+		})
+	}
+}
+
+func TestIndexOf(t *testing.T) {
+	tcs := []struct {
+		name        string
+		s           []string
+		search      string
+		expectFound bool
+		expectIdx   int
+	}{
+		{
+			name:        "empty slice should return false",
+			s:           []string{},
+			search:      "empty",
+			expectFound: false,
+			expectIdx:   0,
+		},
+		{
+			name:        "empty search string should return false",
+			s:           []string{"foo", "bar"},
+			search:      "",
+			expectFound: false,
+			expectIdx:   0,
+		},
+		{
+			name:        "non-matching search string should return false",
+			s:           []string{"foo", "bar"},
+			search:      "doesn'texist",
+			expectFound: false,
+			expectIdx:   0,
+		},
+		{
+			name:        "matching search string should return (true, correct index) 1",
+			s:           []string{"foo", "bar", "baz"},
+			search:      "foo",
+			expectFound: true,
+			expectIdx:   0,
+		},
+		{
+			name:        "matching search string should return (true, correct index) 2",
+			s:           []string{"foo", "bar", "baz"},
+			search:      "baz",
+			expectFound: true,
+			expectIdx:   2,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			found, idx := indexOf(tc.s, tc.search)
+			assert.Equal(t, tc.expectFound, found)
+			assert.Equal(t, tc.expectIdx, idx)
+		})
+	}
+}
---
+++ runner/debug/simple.go
@@ -0,0 +1,139 @@
+package debug
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/hcdiag/op"
+	"github.com/hashicorp/hcdiag/product"
+	"github.com/hashicorp/hcdiag/redact"
+	"github.com/hashicorp/hcdiag/runner"
+)
+
+var _ runner.Runner = SimpleDebug{}
+
+// The "Simple" debug wrapper can be used for Nomad, Vault, and Consul.
+// It only deals with flags common to all three 'debug' commands
+type SimpleDebug struct {
+	Product product.Name `json:"product"`
+	// "Filters" is a generic name for the target/topic/capture option (depending on the product)
+	Filters []string       `json:"filters"`
+	Command runner.Command `json:"command"`
+	// TODO maybe simpleDebugs don't have redactions, since they're always going to be created inside of hcdiag default product runners?
+	// Maybe only custom productDebugs have hcl/redactions?
+	Redactions []*redact.Redact `json:"redactions"`
+}
+
+func (d SimpleDebug) ID() string {
+	return fmt.Sprintf("simpledebug-%s", d.Product)
+}
+
+func NewSimpleDebug(cfg product.Config, filters []string, redactions []*redact.Redact) *SimpleDebug {
+	var cmdStr string
+	var product = cfg.Name
+
+	filterString, err := productFilterString(product, filters)
+	if err != nil {
+		// TODO figure out error handling inside of a runner constructor -- no other runners need this
+		panic(err)
+	}
+
+	switch product {
+	case "nomad":
+		// runner.NewCommand(fmt.Sprintf("nomad operator debug -log-level=TRACE -node-id=all -max-nodes=10 -output=%s -duration=%s -interval=%s", cfg.TmpDir, cfg.DebugDuration, cfg.DebugInterval), "string", cfg.Redactions),
+		cmdStr = fmt.Sprintf("nomad operator debug -log-level=TRACE -duration=%s -interval=%s -node-id=all -max-nodes=100 -output=%s/%s", cfg.DebugDuration, cfg.DebugInterval, cfg.TmpDir, filterString)
+	case "vault":
+		// TODO(dcohen): compress is currently true to maintain backwards compatibility, but I'd like to set this to false since everything gets compressed anyway
+		// TODO loglevel TRACE?
+		cmdStr = fmt.Sprintf("vault debug -compress=true -duration=%s -interval=%s -output=%s/VaultDebug.tar.gz%s", cfg.DebugDuration, cfg.DebugInterval, cfg.TmpDir, filterString)
+	case "consul":
+		cmdStr = fmt.Sprintf("consul debug -duration=%s -interval=%s -output=%s/ConsulDebug%s", cfg.DebugDuration, cfg.DebugInterval, cfg.TmpDir, filterString)
+	}
+
+	return &SimpleDebug{
+		Product: product,
+		Filters: filters,
+		Command: runner.Command{
+			Command:    cmdStr,
+			Format:     "string",
+			Redactions: redactions,
+		},
+		Redactions: redactions,
+	}
+}
+
+func (d SimpleDebug) Run() op.Op {
+	startTime := time.Now()
+
+	o := d.Command.Run()
+	if o.Error != nil {
+		return op.New(d.ID(), o.Result, op.Fail, o.Error, runner.Params(d), startTime, time.Now())
+	}
+
+	return op.New(d.ID(), o.Result, op.Success, nil, runner.Params(d), startTime, time.Now())
+}
+
+// productFilterString takes a product.Name and a slice of filter strings, and produces valid, product-specific filter flags
+// the returned string is in the form "-target=metrics -target=pprof" (for Vault), "-capture=host" (for Consul), or "-event-topic=Allocation" (for Nomad)
+func productFilterString(product product.Name, filters []string) (string, error) {
+	var filterString string
+	var legalFilters []string
+	var optFlag string
+
+	nomadFilters := []string{"*", "ACLToken", "ACLPolicy", "ACLRole", "Job", "Allocation", "Deployment", "Evaluation", "Node", "Service"}
+	nomadOptFlag := "event-topic"
+	vaultFilters := []string{"config", "host", "metrics", "pprof", "replication-status", "server-status"}
+	vaultOptFlag := "target"
+	consulFilters := []string{"agent", "host", "members", "metrics", "logs", "pprof"}
+	consulOptFlag := "capture"
+
+	switch product {
+	case "nomad":
+		legalFilters = nomadFilters
+		optFlag = nomadOptFlag
+	case "vault":
+		legalFilters = vaultFilters
+		optFlag = vaultOptFlag
+	case "consul":
+		legalFilters = consulFilters
+		optFlag = consulOptFlag
+	default:
+		return "", fmt.Errorf("invalid product used in debug.productFilterString(): %s", product)
+	}
+
+	for _, f := range filters {
+		// Skip empty entries
+		if f == "" {
+			continue
+		}
+
+		found, idx := indexOf(legalFilters, f)
+		if found {
+			// includes leading space
+			// legalFilters[idx] (as opposed to s) ensures the correct filter option capitalization
+			filterString = fmt.Sprintf("%s -%s=%s", filterString, optFlag, legalFilters[idx])
+		} else {
+			return "", fmt.Errorf("invalid filter string (%s) for %s used in debug.productFilterString()", f, product)
+		}
+	}
+
+	return filterString, nil
+}
+
+// This can't really be necessary in Go, right?
+// Searches s for presence of val: returns true and index of val, or false and 0
+func indexOf(s []string, val string) (found bool, idx int) {
+	// Early exit
+	if len(s) == 0 || len(val) == 0 {
+		return false, 0
+	}
+
+	for i, v := range s {
+		// compare case-insensitive strings to avoid erroring on capitalization
+		if strings.EqualFold(v, val) {
+			return true, i
+		}
+	}
+	return false, 0
+}
