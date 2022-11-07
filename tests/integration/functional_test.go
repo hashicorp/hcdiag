c4e211afb1b5633c85d0b65326293a43382e3716
David Cohen <dcohen@hashicorp.com>
2022-11-07 10:04:52 -0500

    try to gently fix functional tests

--- tests/integration/functional_test.go
+++ tests/integration/functional_test.go
@@ -76,7 +76,7 @@ func TestFunctional(t *testing.T) {
 				},
 				"vault-unix": {
 					flags:    []string{"-vault"},
-					outFiles: []string{"VaultDebug.tar.gz"},
+					outFiles: []string{"VaultDebug*.tar.gz"},
 					// TODO(gulducat): de-unique-ize when `vault debug` is fixed on windows
 					// dave's pr: https://github.com/hashicorp/vault/pull/14399
 					skip: runtime.GOOS == "windows",
