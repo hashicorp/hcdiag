7540d3ba4bced0fe9b9490c2de7c4c786553ad70
David Cohen <dcohen@hashicorp.com>
2022-11-11 12:57:14 -0500

    add ConsulDebugs to hcl, and fix hcl/json keys for VaultDebugs

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -67,6 +67,7 @@ type Product struct {
 	DockerLogs   []DockerLog   `hcl:"docker-log,block" json:"docker_log,omitempty"`
 	JournaldLogs []JournaldLog `hcl:"journald-log,block" json:"journald_log,omitempty"`
 	VaultDebugs  []VaultDebug  `hcl:"vault-debug,block" json:"vault_debug,omitempty"`
+	ConsulDebugs []ConsulDebug `hcl:"consul-debug,block" json:"consul_debug,omitempty"`
 	Excludes     []string      `hcl:"excludes,optional" json:"excludes,omitempty"`
 	Selects      []string      `hcl:"selects,optional" json:"selects,omitempty"`
 	Redactions   []Redact      `hcl:"redact,block" json:"redactions,omitempty"`
@@ -88,6 +89,7 @@ type Do struct {
 	DockerLogs   []DockerLog   `hcl:"docker-log,block" json:"docker_log,omitempty"`
 	JournaldLogs []JournaldLog `hcl:"journald-log,block" json:"journald_log,omitempty"`
 	VaultDebugs  []VaultDebug  `hcl:"vault-debug,block" json:"vault_debug,omitempty"`
+	ConsulDebugs []ConsulDebug `hcl:"consul-debug,block" json:"consul_debug,omitempty"`
 
 	// Filters
 	// Excludes     []string      `hcl:"excludes,optional" json:"excludes,omitempty"`
@@ -112,7 +114,8 @@ type DoSync struct {
 	Copies       []Copy        `hcl:"copy,block" json:"copies,omitempty"`
 	DockerLogs   []DockerLog   `hcl:"docker-log,block" json:"docker_log,omitempty"`
 	JournaldLogs []JournaldLog `hcl:"journald-log,block" json:"journald_log,omitempty"`
-	VaultDebugs  []VaultDebug  `hcl:"vaultdebug,block" json:"vaultdebug,omitempty"`
+	VaultDebugs  []VaultDebug  `hcl:"vault-debug,block" json:"vault_debug,omitempty"`
+	ConsulDebugs []ConsulDebug `hcl:"consul-debug,block" json:"consul_debug,omitempty"`
 
 	// Filters
 	// Excludes     []string      `hcl:"excludes,optional" json:"excludes,omitempty"`
@@ -172,6 +175,14 @@ type VaultDebug struct {
 	MetricsInterval string   `hcl:"metrics-interval,optional" json:"metrics_interval"`
 	Targets         []string `hcl:"targets,optional" json:"targets"`
 	Redactions      []Redact `hcl:"redact,block" json:"redactions"`
+}
+
+type ConsulDebug struct {
+	Archive    string   `hcl:"archive" json:"archive"`
+	Duration   string   `hcl:"duration,optional" json:"duration"`
+	Interval   string   `hcl:"interval,optional" json:"interval"`
+	Captures   []string `hcl:"captures,optional" json:"captures"`
+	Redactions []Redact `hcl:"redact,block" json:"redactions"`
 }
 
 // Parse takes a file path and decodes the file from disk into HCL types.
@@ -238,6 +249,12 @@ func BuildRunnersWithContext[T Blocks](ctx context.Context, config T, tmpDir str
 			return nil, err
 		}
 		runners = append(runners, vaultDebugs...)
+
+		consulDebugs, err := mapConsulDebugs(ctx, cfg.ConsulDebugs, tmpDir, debugDuration, debugInterval, redactions)
+		if err != nil {
+			return nil, err
+		}
+		runners = append(runners, consulDebugs...)
 
 		// Build commands and shells
 		commands, err := mapCommands(ctx, cfg.Commands, redactions)
@@ -462,6 +479,31 @@ func mapVaultDebugs(ctx context.Context, cfgs []VaultDebug, tmpDir string, debug
 		}
 
 		runners[i] = debug.NewVaultDebug(cfg, tmpDir, debugDuration, debugInterval)
+	}
+	return runners, nil
+}
+
+func mapConsulDebugs(ctx context.Context, cfgs []ConsulDebug, tmpDir string, debugDuration time.Duration, debugInterval time.Duration, redactions []*redact.Redact) ([]runner.Runner, error) {
+	runners := make([]runner.Runner, len(cfgs))
+
+	for i, d := range cfgs {
+		runnerRedacts, err := MapRedacts(d.Redactions)
+		if err != nil {
+			return nil, err
+		}
+		// Prepend runner-level redactions to those passed in
+		runnerRedacts = append(runnerRedacts, redactions...)
+
+		// Create the runner
+		cfg := debug.ConsulDebugConfig{
+			Archive:    d.Archive,
+			Duration:   d.Duration,
+			Interval:   d.Interval,
+			Captures:   d.Captures,
+			Redactions: runnerRedacts,
+		}
+
+		runners[i] = debug.NewConsulDebug(cfg, tmpDir, debugDuration, debugInterval)
 	}
 	return runners, nil
 }
